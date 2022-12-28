236124b4761b6832ce571c8c3dde1724645550bc
CSarroca <sarroca93@gmail.com>
2022-12-28 21:46:25 +0100

    Modified data for components

--- src/components/PodcastList/PodcastList.jsx
+++ src/components/PodcastList/PodcastList.jsx
@@ -1,31 +1,12 @@
-import { useQuery } from "react-query";
 import PodcastCard from "../PodcastCard/PodcastCard";
 import PodcastListSyled from "./PodcastListStyled";
 
-const PodcastList = () => {
-  const getPodcasts = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}`);
-
-    return response.json();
-  };
-
-  const { data, status } = useQuery(["podcasts"], getPodcasts, {
-    staleTime: 86400000,
-  });
-
-  if (status === "loading") {
-    return <p>Loading podcasts...</p>;
-  }
-
-  if (status === "error") {
-    return <p>Error</p>;
-  }
-
+const PodcastList = ({ podcasts }) => {
   return (
     <PodcastListSyled>
-      {data.feed.entry.map((pod) => (
-        <li className="podcast-list__item" key={pod.id.attributes["im:id"]}>
-          <PodcastCard podcast={pod} />
+      {podcasts.map((podcast) => (
+        <li className="podcast-list__item" key={podcast.id}>
+          <PodcastCard podcast={podcast} />
         </li>
       ))}
     </PodcastListSyled>
