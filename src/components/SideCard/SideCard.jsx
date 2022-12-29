9142a4f637a0a659959bb7554a5d075ffa85401a
CSarroca <sarroca93@gmail.com>
2022-12-29 19:17:38 +0100

    Created SideCard component

---
+++ src/components/SideCard/SideCard.jsx
@@ -0,0 +1,23 @@
+const SideCard = (podcast) => {
+  return (
+    <>
+      <img
+        className="side-card__cover"
+        src={podcast.image}
+        alt={`${podcast.name} podcast cover`}
+        height={55}
+        width={55}
+      />
+      <h2 className="side-card__name">{podcast.name}</h2>
+      <h3 className="side-card__author">Author: {podcast.author}</h3>
+
+      <p className="side-text">
+        {" "}
+        Description:
+        {podcast.description}
+      </p>
+    </>
+  );
+};
+
+export default SideCard;
