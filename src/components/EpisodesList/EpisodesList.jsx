929ad647b24c1f5fa627d500e3d7fa1180330264
CSarroca <sarroca93@gmail.com>
2023-01-02 16:33:30 +0100

    Created function to transform milisec to minutes and sec

--- src/components/EpisodesList/EpisodesList.jsx
+++ src/components/EpisodesList/EpisodesList.jsx
@@ -1,6 +1,11 @@
 import EpisodeListStyled from "./EpisodeListStyled";
 
 const EpisodeList = ({ episodeList }) => {
+  const millisToMinutesAndSeconds = (millis) => {
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`; //ES6 interpolated literals/template literals //If seconds is less than 10 put a zero in front.return`$minutes:$(seconds <10?"0":"")$seconds`;
+  };
   return (
     <EpisodeListStyled>
       <span className="episodeList__head"> Episodes: 110</span>
@@ -17,7 +22,7 @@ const EpisodeList = ({ episodeList }) => {
             <tr key={episode.trackId}>
               <td>{episode.title}</td>
               <td>{episode.date}</td>
-              <td>{episode.durations}</td>
+              <td>{millisToMinutesAndSeconds(episode.duration)}</td>
             </tr>
           ))}
         </tbody>
