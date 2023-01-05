da5372b3d1feea135918f67b83ecdecea77e0f07
CSarroca <sarroca93@gmail.com>
2023-01-05 12:46:50 +0100

    Added function to show loader when view changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
+import { useEffect, useState } from "react";
 import GlobalLoadingIndicator from "../LoadingIndicator/LoadingIndicator";
 import HeaderStyled from "./HeaderStyled";
 
-const { NavLink } = require("react-router-dom");
+const { NavLink, useLocation } = require("react-router-dom");
 
 const Header = () => {
+  let location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    setTimeout(function () {
+      setIsLoading(false);
+    }, 1000);
+  }, [location]);
+
   return (
     <HeaderStyled>
       <NavLink className="link" to="/">
         <h1>Podcaster</h1>
       </NavLink>
-
-      <GlobalLoadingIndicator />
+      <GlobalLoadingIndicator isLoading={isLoading} />
     </HeaderStyled>
   );
 };
